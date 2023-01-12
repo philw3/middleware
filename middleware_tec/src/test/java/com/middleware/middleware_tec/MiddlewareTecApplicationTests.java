a2a6020c203db635d587aca1407267382c03ba2a
PhilippWalker <33124244+philw3@users.noreply.github.com>
2023-01-12 12:43:17 +0100

    test

--- middleware_tec/src/test/java/com/middleware/middleware_tec/MiddlewareTecApplicationTests.java
+++ middleware_tec/src/test/java/com/middleware/middleware_tec/MiddlewareTecApplicationTests.java
@@ -1,12 +1,12 @@
 package com.middleware.middleware_tec;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MiddlewareTecApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
